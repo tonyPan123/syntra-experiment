fdd4febcc57f4d3501f2eb361cbbb0f786780927
Tony Pan <panjia2001@gmail.com>
2025-08-01 20:00:17 -0500

    fix bug

--- gcc/playground/gcc-experiment.sh
+++ gcc/playground/gcc-experiment.sh
@@ -3,19 +3,19 @@ set -e
 
 mm-delay 25 bash -c "python3 update_config.py --ip \$MAHIMAHI_BASE --port $1 --sconfig sender.json --rconfig receiver.json"
 if [ "$4" = "trace" ]; then
-    timeout 10m mm-delay 25 mm-link $2 $3  -- bash -c "sleep 3 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
+    timeout 10m mm-delay 25 mm-link $2 $3  -- bash -c "sleep 10 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
     sleep 1
     timeout 10m ./peerconnection_gcc receiver.json > receiver.log 2>&1 &
 else 
     if [ "$4" = "shallow" ]; then
-        timeout 3m mm-delay 25 mm-link $2 $3 --uplink-queue=droptail --uplink-queue-args="bytes=10500"  -- bash -c "sleep 3 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
+        timeout 3m mm-delay 25 mm-link $2 $3 --uplink-queue=droptail --uplink-queue-args="bytes=10500"  -- bash -c "sleep 10 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
     else 
-        timeout 3m mm-delay 25 mm-link $2 $3  -- bash -c "sleep 3 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
+        timeout 3m mm-delay 25 mm-link $2 $3  -- bash -c "sleep 10 ; ./peerconnection_gcc sender.json > sender.log 2>&1" &
     fi
     sleep 1
     timeout 3m ./peerconnection_gcc receiver.json > receiver.log 2>&1 &
 fi
 wait 
 python3 example-scripts/e2e_delay.py -r receiver.log
-python3 webrtc-quality.py -o testmedia/benchmark-barcode.y4m -n outvideo.y4m
+python3 webrtc-quality.py -o testmedia/test-barcode.y4m -n outvideo.y4m
 python3 webrtc-rate.py -s sender.log -r receiver.log
--- vegas/playground/vegas-experiment.sh
+++ vegas/playground/vegas-experiment.sh
@@ -5,15 +5,15 @@ cp ../modules/third_party/onnxinfer/lib/* .
 export LD_LIBRARY_PATH=.
 mm-delay 25 bash -c "python3 update_config.py --ip \$MAHIMAHI_BASE --port $1 --sconfig sender.json --rconfig receiver.json"
 if [ "$4" = "trace" ]; then
-    timeout 10m mm-delay 25 mm-link $2 $3  -- bash -c "export LD_LIBRARY_PATH=.; sleep 3 ; ./peerconnection_serverless sender.json > sender.log 2>&1" &
+    timeout 10m mm-delay 25 mm-link $2 $3  -- bash -c "export LD_LIBRARY_PATH=.; sleep 10 ; ./peerconnection_serverless sender.json > sender.log 2>&1" &
     sleep 1
     timeout 10m ./peerconnection_serverless receiver.json > receiver.log 2>&1 &
 else 
-    timeout 3m mm-delay 25 mm-link $2 $3  -- bash -c "export LD_LIBRARY_PATH=.; sleep 3 ; ./peerconnection_serverless sender.json > sender.log 2>&1" &
+    timeout 3m mm-delay 25 mm-link $2 $3  -- bash -c "export LD_LIBRARY_PATH=.; sleep 10 ; ./peerconnection_serverless sender.json > sender.log 2>&1" &
     sleep 1
     timeout 3m ./peerconnection_serverless receiver.json > receiver.log 2>&1 &
 fi
 wait 
 python3 example-scripts/e2e_delay.py -r receiver.log
-python3 webrtc-quality.py -o testmedia/benchmark-barcode.y4m -n outvideo.y4m
+python3 webrtc-quality.py -o testmedia/test-barcode.y4m -n outvideo.y4m
 python3 webrtc-rate.py -s sender.log -r receiver.log
