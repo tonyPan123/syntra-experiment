fb359975665747e8f7e066bea0a43d67d8fe15ad
Tony Pan <panjia2001@gmail.com>
2025-08-02 21:46:55 -0500

    fix format of figure4

--- figure4.sh
+++ figure4.sh
@@ -9,49 +9,49 @@ port3=$((base + 3))
 pushd alfalfa/src/salsify
 echo "Experiment for Syntra----------------------------"
 echo "LTE_ATT--------"
-./syntra-trace-experiment.sh $1 $port1 /usr/share/mahimahi/traces/ATT-LTE-driving.up /usr/share/mahimahi/traces/ATT-LTE-driving.down 
+./syntra-trace-experiment.sh $1 $port1 $2/ATT-LTE-driving.up $2/ATT-LTE-driving.down 
 sleep 5
 echo "TMobile_UMTS--------"
-./syntra-trace-experiment.sh $1 $port2 /usr/share/mahimahi/traces/TMobile-UMTS-driving.up /usr/share/mahimahi/traces/TMobile-UMTS-driving.down 
+./syntra-trace-experiment.sh $1 $port2 $2/TMobile-UMTS-driving.up $2/TMobile-UMTS-driving.down 
 sleep 5
 echo "Verizon_LTE--------"
-./syntra-trace-experiment.sh $1 $port3 /usr/share/mahimahi/traces/Verizon-LTE-driving.up /usr/share/mahimahi/traces/Verizon-LTE-driving.down
+./syntra-trace-experiment.sh $1 $port3 $2/Verizon-LTE-driving.up $2/Verizon-LTE-driving.down
 sleep 5
 
 echo "Experiment for Salsify----------------------------"
 echo "LTE_ATT--------"
-./salsify-experiment.sh $1 $port1 /usr/share/mahimahi/traces/ATT-LTE-driving.up /usr/share/mahimahi/traces/ATT-LTE-driving.down trace
+./salsify-experiment.sh $1 $port1 $2/ATT-LTE-driving.up $2/ATT-LTE-driving.down trace
 sleep 5
 echo "TMobile_UMTS--------"
-./salsify-experiment.sh $1 $port2 /usr/share/mahimahi/traces/TMobile-UMTS-driving.up /usr/share/mahimahi/traces/TMobile-UMTS-driving.down trace
+./salsify-experiment.sh $1 $port2 $2/TMobile-UMTS-driving.up $2/TMobile-UMTS-driving.down trace
 sleep 5
 echo "Verizon_LTE--------"
-./salsify-experiment.sh $1 $port3 /usr/share/mahimahi/traces/Verizon-LTE-driving.up /usr/share/mahimahi/traces/Verizon-LTE-driving.down trace
+./salsify-experiment.sh $1 $port3 $2/Verizon-LTE-driving.up $2/Verizon-LTE-driving.down trace
 sleep 5
 popd
 
 pushd gcc/playground
 echo "Experiment for WebRTC-gcc----------------------------"
 echo "LTE_ATT--------"
-./gcc-experiment.sh $port1 /usr/share/mahimahi/traces/ATT-LTE-driving.up /usr/share/mahimahi/traces/ATT-LTE-driving.down trace
+./gcc-experiment.sh $port1 $2/ATT-LTE-driving.up $2/ATT-LTE-driving.down trace
 sleep 5
 echo "TMobile_UMTS--------"
-./gcc-experiment.sh $port2 /usr/share/mahimahi/traces/TMobile-UMTS-driving.up /usr/share/mahimahi/traces/TMobile-UMTS-driving.down trace
+./gcc-experiment.sh $port2 $2/TMobile-UMTS-driving.up $2/TMobile-UMTS-driving.down trace
 sleep 5
 echo "Verizon_LTE--------"
-./gcc-experiment.sh $port3 /usr/share/mahimahi/traces/Verizon-LTE-driving.up /usr/share/mahimahi/traces/Verizon-LTE-driving.down trace
+./gcc-experiment.sh $port3 $2/Verizon-LTE-driving.up $2/Verizon-LTE-driving.down trace
 sleep 5
 popd
 
 pushd vegas/playground
 echo "Experiment for WebRTC-Vegas----------------------------"
 echo "LTE_ATT--------"
-./vegas-experiment.sh $port1 /usr/share/mahimahi/traces/ATT-LTE-driving.up /usr/share/mahimahi/traces/ATT-LTE-driving.down trace
+./vegas-experiment.sh $port1 $2/ATT-LTE-driving.up $2/ATT-LTE-driving.down trace
 sleep 5
 echo "TMobile_UMTS--------"
-./vegas-experiment.sh $port2 /usr/share/mahimahi/traces/TMobile-UMTS-driving.up /usr/share/mahimahi/traces/TMobile-UMTS-driving.down trace
+./vegas-experiment.sh $port2 $2/TMobile-UMTS-driving.up $2/TMobile-UMTS-driving.down trace
 sleep 5
 echo "Verizon_LTE--------"
-./vegas-experiment.sh $port3 /usr/share/mahimahi/traces/Verizon-LTE-driving.up /usr/share/mahimahi/traces/Verizon-LTE-driving.down trace
+./vegas-experiment.sh $port3 $2/Verizon-LTE-driving.up $2/Verizon-LTE-driving.down trace
 sleep 5
 popd
